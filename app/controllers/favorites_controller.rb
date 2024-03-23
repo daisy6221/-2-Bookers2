d179e3e10a4d3b5a583ff3d2e888f76006098053
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-03-23 08:57:19 +0000

    [Add] 5-3 いいね機能追加

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,19 @@
+class FavoritesController < ApplicationController
+
+  def create
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: book.id)
+    favorite.save
+    previous_page_url = request.referer
+    redirect_to previous_page_url
+  end
+
+  def destroy
+    book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: book.id)
+    favorite.destroy
+    previous_page_url = request.referer
+    redirect_to previous_page_url
+  end
+
+end
