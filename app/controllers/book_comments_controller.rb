8fa30d3c05d50ca4aff4b3e94c32e00e95b4d6c5
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-03-23 14:57:52 +0000

    [Remove] 5-3 コメント機能の不具合修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,7 +2,8 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.BookComment.new(book_id: book_id)
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
     comment.save
     redirect_to book_path(book.id)
   end
@@ -12,4 +13,9 @@ class BookCommentsController < ApplicationController
     redirect_to book_path(params[:book_id])
   end
 
+private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
 end
